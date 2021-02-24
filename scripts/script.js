bb033049d1e9eb5e54b860dda23b3619a4a48362
Maroli <fernando.sch6@gmail.com>
2021-02-24 17:38:27 +0000

    to github io

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $("#grid-1").hover(function() {
+        $("#grid-1 img").css("opacity", "0.6");
+        $("#grid-1 .btn-read-more").css("opacity", "1");
+    }, function() {
+        $("#grid-1 img").css("opacity", "1");
+        $("#grid-1 .btn-read-more").css("opacity", "0");
+    });
+
+    $("#grid-2").hover(function() {
+        $("#grid-2 img").css("opacity", "0.6");
+        $("#grid-2 .btn-read-more").css("opacity", "1");
+    }, function() {
+        $("#grid-2 img").css("opacity", "1");
+        $("#grid-2 .btn-read-more").css("opacity", "0");
+    });
+
+    $("#grid-3").hover(function() {
+        $("#grid-3 img").css("opacity", "0.6");
+        $("#grid-3 .btn-read-more").css("opacity", "1");
+    }, function() {
+        $("#grid-3 img").css("opacity", "1");
+        $("#grid-3 .btn-read-more").css("opacity", "0");
+    });
+
+    $("#grid-4").hover(function() {
+        $("#grid-4 img").css("opacity", "0.6");
+        $("#grid-4 .btn-read-more").css("opacity", "1");
+    }, function() {
+        $("#grid-4 img").css("opacity", "1");
+        $("#grid-4 .btn-read-more").css("opacity", "0");
+    });
+
+    $("#grid-5").hover(function() {
+        $("#grid-5 img").css("opacity", "0.6");
+        $("#grid-5 .btn-read-more").css("opacity", "1");
+    }, function() {
+        $("#grid-5 img").css("opacity", "1");
+        $("#grid-5 .btn-read-more").css("opacity", "0");
+    });
+
+    $("#grid-6").hover(function() {
+        $("#grid-6 img").css("opacity", "0.6");
+        $("#grid-6 .btn-read-more").css("opacity", "1");
+    }, function() {
+        $("#grid-6 img").css("opacity", "1");
+        $("#grid-6 .btn-read-more").css("opacity", "0");
+    });
+});
